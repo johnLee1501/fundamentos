0bf62edb1ca0a1946c44c82b98a17cda8afb9f66
jhonj <johnvega@wposs.com>
2021-07-23 00:20:29 -0500

    Initial commit

---
+++ src/test/java/platzi/spring/fundamentos/FundamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package platzi.spring.fundamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
